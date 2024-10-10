43a9caf2e0f99ab5ba2c0b565aabf3d4eac71c54
lab <lab@lab1-22.NUCFD.edu.local>
2024-10-10 10:42:08 +0500

    added addtion and multiplication

---
+++ SCD1/src/pkg/Calculator.java
@@ -0,0 +1,15 @@
+// Calculator.java
+package pkg;
+
+public class Calculator {
+    
+    // Method for addition
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    // Method for multiplication
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+}
